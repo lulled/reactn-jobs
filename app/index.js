119c7575eb204cd9d4e8b7775e20aa54ebf7115a
lule ahmed <ahmedlule31@gmail.com>
2023-03-23 20:15:39 +0300

    pushing to git

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import 'expo-router/entry';
+import { View, Text } from 'react-native-web';
+
+const Home = () => {
+	return (
+		<View>
+			<Text>hello</Text>
+		</View>
+	);
+};
+export default Home;
